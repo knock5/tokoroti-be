9f36c47a1e322d7117b31298f7472965cfccbb49
Miftachul Huda <miftachulhd5@gmail.com>
2025-12-02 16:17:12 +0700

    setup project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors();
+  app.setGlobalPrefix('api/v1');
+
+  const port = Number(process.env.PORT);
+
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}/api/v1`);
 }
 bootstrap();
